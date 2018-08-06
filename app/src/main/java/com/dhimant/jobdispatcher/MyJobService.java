91e04c19e44bed822b185d94466440c42cdb1741
dhimantdesai <dhimant1990@gmail.com>
2018-08-06 20:27:14 +0530

    Initial commit

---
+++ app/src/main/java/com/dhimant/jobdispatcher/MyJobService.java
@@ -0,0 +1,26 @@
+package com.dhimant.jobdispatcher;
+
+import android.util.Log;
+
+import com.firebase.jobdispatcher.JobParameters;
+import com.firebase.jobdispatcher.JobService;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+public class MyJobService extends JobService {
+    private static final String TAG = "MyJobService";
+
+    @Override
+    public boolean onStartJob(JobParameters jobParameters) {
+        String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
+        Log.d(TAG, currentDateTimeString);
+        return false;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters jobParameters) {
+        Log.d(TAG, "Job cancelled!");
+        return false;
+    }
+}
